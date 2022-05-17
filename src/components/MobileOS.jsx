e956a27b80b6f55ad827a3a47e3a1345590927bb
Bhavesh2302 <jainbhavesh888@gmail.com>
2022-05-17 23:29:04 +0530

    added static card

---
+++ src/components/MobileOS.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+const Mobile = () =>{
+
+return( 
+<div>
+
+<h2>Mobile Operating System</h2>
+<ul>
+<li> Android</li>
+<li>Blckberry</li>
+<li>iPhone</li>
+<li>Windows</li>
+
+</ul>
+</div>
+
+)
+}
+
+export default Mobile
+
+
+const Mobile1 = () =>{
+
+    return( 
+    <div>
+   
+    <h2>Mobile Manufactures</h2>
+    <ul>
+    <li> Samsung</li>
+    <li>HTC</li>
+    <li>Micromax</li>
+    <li>Apple</li>
+    </ul>
+    </div>
+    
+    
+    )
+
+
+    }
+
+    export {Mobile1}
